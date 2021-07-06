625ff07d37e4464888087a5ac1dc5c778bc5bcc2
Aaron Martinez <aarondev98@gmail.com>
2021-07-06 19:20:57 -0400

    Add project files.

---
+++ Practica IV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Practica_IV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
